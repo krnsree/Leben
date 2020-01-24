460b61e29a2f7e3dca7f12b7790a9c2c2d6797b2
krnsree <krnimpuse@gmail.com>
2020-01-24 21:12:39 +0530

    LEBEN App

---
+++ app/src/main/java/com/example/project/ClinicFragment.java
@@ -0,0 +1,24 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class ClinicFragment extends Fragment {
+
+    public ClinicFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_clinic, container, false);
+    }
+}
